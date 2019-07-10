489072cdd5acb80d33f8354ac3cbecd3e9c14a7e
Pierre-Alain <ouvrard.pierre.alain@gmail.com>
2019-07-10 20:49:46 +0900

    test withdrawable balances, remove simple transfer test

--- tests/ethaergo_wallet/test_bridge_transfers.py
+++ tests/ethaergo_wallet/test_bridge_transfers.py
@@ -19,8 +19,7 @@ def test_standard_token_transfer(bridge_wallet):
     )
     assert balance_origin_after == balance_origin_before - 5*10**18
     bridge_wallet.mint_to_eth(
-        'aergo-local', 'eth-poa-local', 'token1',
-        '0xfec3c905bcd3d9a5471452e53f82106844cb1e76',
+        'aergo-local', 'eth-poa-local', 'token1', eth_user,
         lock_height, privkey_pwd='1234'
     )
     balance_destination_after, _ = bridge_wallet.get_balance_eth(
@@ -135,3 +134,115 @@ def test_aergo_erc20_unfreeze(bridge_wallet):
     )
     assert balance_origin_after2 == balance_destination_after - 5*10**18
     assert balance_destination_after2 == balance_origin_before
+
+
+def test_aergo_unfreezeable(bridge_wallet):
+    eth_user = bridge_wallet.config_data('wallet-eth', 'default', 'addr')
+    aergo_user = bridge_wallet.config_data('wallet', 'default', 'addr')
+
+    withdrawable, pending = bridge_wallet.unfreezeable(
+        'eth-poa-local', 'aergo-local', aergo_user
+    )
+    before_transfer = withdrawable + pending
+    lock_height, _ = bridge_wallet.lock_to_aergo(
+        'eth-poa-local', 'aergo-local', 'aergo_erc20',
+        5*10**18, aergo_user, privkey_pwd='1234'
+    )
+    withdrawable, pending = bridge_wallet.unfreezeable(
+        'eth-poa-local', 'aergo-local', aergo_user
+    )
+    assert withdrawable + pending - before_transfer == 5*10**18
+    # send tokens back to origin
+    bridge_wallet.unfreeze(
+        'eth-poa-local', 'aergo-local', aergo_user, lock_height,
+        privkey_pwd='1234'
+    )
+    freeze_height, _ = bridge_wallet.freeze(
+        'aergo-local', 'eth-poa-local', 5*10**18, eth_user,
+        privkey_pwd='1234'
+    )
+    bridge_wallet.unlock_to_eth(
+        'aergo-local', 'eth-poa-local', 'aergo_erc20',
+        eth_user, freeze_height,
+        privkey_pwd='1234'
+    )
+
+
+def test_token_withdrawable(bridge_wallet):
+    eth_user = bridge_wallet.config_data('wallet-eth', 'default', 'addr')
+    aergo_user = bridge_wallet.config_data('wallet', 'default', 'addr')
+
+    withdrawable, pending = bridge_wallet.minteable_to_eth(
+        'aergo-local', 'eth-poa-local', 'token1', eth_user
+    )
+    before_transfer = withdrawable + pending
+    lock_height, _ = bridge_wallet.lock_to_eth(
+        'aergo-local', 'eth-poa-local', 'token1', 5*10**18,
+        eth_user, privkey_pwd='1234'
+    )
+    withdrawable, pending = bridge_wallet.minteable_to_eth(
+        'aergo-local', 'eth-poa-local', 'token1', eth_user
+    )
+    assert withdrawable + pending - before_transfer == 5*10**18
+    bridge_wallet.mint_to_eth(
+        'aergo-local', 'eth-poa-local', 'token1', eth_user,
+        lock_height, privkey_pwd='1234'
+    )
+    withdrawable, pending = bridge_wallet.unlockeable_to_aergo(
+        'eth-poa-local', 'aergo-local', 'token1', aergo_user
+    )
+    before_transfer = withdrawable + pending
+    burn_height, _ = bridge_wallet.burn_to_aergo(
+        'eth-poa-local', 'aergo-local', 'token1',
+        5*10**18, aergo_user, privkey_pwd='1234'
+    )
+    withdrawable, pending = bridge_wallet.unlockeable_to_aergo(
+        'eth-poa-local', 'aergo-local', 'token1', aergo_user
+    )
+    assert withdrawable + pending - before_transfer == 5*10**18
+    bridge_wallet.unlock_to_aergo(
+        'eth-poa-local', 'aergo-local', 'token1', aergo_user, burn_height,
+        privkey_pwd='1234'
+    )
+
+
+def test_erc20_withdrawable(bridge_wallet):
+    eth_user = bridge_wallet.config_data('wallet-eth', 'default', 'addr')
+    aergo_user = bridge_wallet.config_data('wallet', 'default', 'addr')
+
+    withdrawable, pending = bridge_wallet.minteable_to_aergo(
+        'eth-poa-local', 'aergo-local', 'test_erc20', aergo_user
+    )
+    before_transfer = withdrawable + pending
+    lock_height, _ = bridge_wallet.lock_to_aergo(
+        'eth-poa-local', 'aergo-local', 'test_erc20',
+        5*10**18, aergo_user, privkey_pwd='1234'
+    )
+    withdrawable, pending = bridge_wallet.minteable_to_aergo(
+        'eth-poa-local', 'aergo-local', 'test_erc20', aergo_user
+    )
+
+    assert withdrawable + pending - before_transfer == 5*10**18
+
+    bridge_wallet.mint_to_aergo(
+        'eth-poa-local', 'aergo-local', 'test_erc20', aergo_user, lock_height,
+        privkey_pwd='1234'
+    )
+
+    withdrawable, pending = bridge_wallet.unlockeable_to_eth(
+        'aergo-local', 'eth-poa-local', 'test_erc20', eth_user,
+    )
+    before_transfer = withdrawable + pending
+    burn_height, _ = bridge_wallet.burn_to_eth(
+        'aergo-local', 'eth-poa-local', 'test_erc20', 5*10**18, eth_user,
+        privkey_pwd='1234'
+    )
+    withdrawable, pending = bridge_wallet.unlockeable_to_eth(
+        'aergo-local', 'eth-poa-local', 'test_erc20', eth_user,
+    )
+    assert withdrawable + pending - before_transfer == 5*10**18
+    bridge_wallet.unlock_to_eth(
+        'aergo-local', 'eth-poa-local', 'test_erc20',
+        eth_user, burn_height,
+        privkey_pwd='1234'
+    )
