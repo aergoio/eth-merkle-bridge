6a91e6a306287bb0eb83126b85ce9d73ceee79e8
Pierre-Alain <ouvrard.pierre.alain@gmail.com>
2020-01-06 16:59:55 +0900

    release v0.3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="eth-merkle-bridge",
-    version="0.3.3",
+    version="0.3.4",
     author="Pierre-Alain Ouvrard",
     author_email="ouvrard.pierre.alain@gmail.com",
     description="POC implementation of the eth-merkle-bridge",
